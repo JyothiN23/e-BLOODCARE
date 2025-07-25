a6af63a331745f8a4ec403c61cbc21700b3e9ade
JyothiN23 <jyothinjyothin726@gmail.com>
2025-07-25 12:47:14 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,14 +1,31 @@
 from django import forms
-
+from django.contrib.auth.models import User
 from . import models
 
-
-class BloodForm(forms.ModelForm):
+BLO0D_GROUP_CHOICES = [
+    ('A+', 'A+'),
+    ('A-', '-A'),
+    ('B+', 'B+'),
+    ('-B', '-B'),
+    ('AB+', 'AB+'),
+    ('AB-', 'AB-'),
+    ('O+', 'O+'),
+    ('O-', 'O-'),
+]
+class DonorUserForm(forms.ModelForm):
     class Meta:
-        model=models.Stock
-        fields=['bloodgroup','unit']
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
 
-class RequestForm(forms.ModelForm):
+class DonorForm(forms.ModelForm):
     class Meta:
-        model=models.BloodRequest
-        fields=['patient_name','patient_age','reason','bloodgroup','unit']
+        model=models.Donor
+        fields=['bloodgroup','address','mobile','profile_pic']
+
+class DonationForm(forms.ModelForm):
+    class Meta:
+        model=models.BloodDonate
+        fields=['age','bloodgroup','disease','unit']
